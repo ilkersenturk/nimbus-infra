49ac1eb07cb51245594488e272c02f713efd5974
ilker senturk <68521925+ilkersenturk@users.noreply.github.com>
2025-05-04 15:21:15 +0000

    added web_sg_id in output

--- infra/modules/webserver/output.tf
+++ infra/modules/webserver/output.tf
@@ -2,3 +2,6 @@ output "public_ip" {
   description = "Public IP address of the web server"
   value       = aws_instance.web.public_ip
 }
+output "web_sg_id" {
+  value = aws_security_group.web_sg.id
+}
